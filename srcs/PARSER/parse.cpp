b8af82ed225b622ce2f3a70be8cc62654db464d7
pfuentes <pfuentes@student.42madrid.com>
2024-02-01 13:09:15 +0100

    limpiar

--- srcs/PARSER/parse.cpp
+++ srcs/PARSER/parse.cpp
@@ -4,24 +4,21 @@ bool	tokenizeFile(const char *file, std::vector<t_token> &tokens, std::string &d
 {
 	std::fstream	readTokens(file);
 	if (readTokens.fail())
-	{
-		//std::cout << "Invalid file!" << std::endl;
 		return (false);
-	}
 	std::string	tokenStr;
 	t_token	token;
 	int	start;
 	int	end;
 	int	flag;
 	int	brackets = 0;
-	while (readTokens >> tokenStr) //!readTokens.eof()
+	while (readTokens >> tokenStr)
 	{	
 		start = 0;
 		end = 0;
 		flag = 0;
 		for (size_t i = 0; i < tokenStr.length(); i++)
 		{
-			for (size_t j = 0; j < del.length(); j++) //quiza hacer una funcion aparte que busque un char en un str, como un "alfabeto"
+			for (size_t j = 0; j < del.length(); j++)
 			{
 				if (tokenStr[i] == del[j])
 				{
@@ -52,13 +49,13 @@ bool	tokenizeFile(const char *file, std::vector<t_token> &tokens, std::string &d
 			{
 				if (tokens[tokens.size() - 2].value == "{" && tokens[tokens.size() - 1].value == "}")
 				{
-					std::cout << "Error: contexto está vacío" << std::endl;
+					std::cerr << "Error: contexto está vacío" << std::endl;
 					return (false);
 				}
 				if ((tokens[tokens.size() - 2].value == "{" || tokens[tokens.size() - 2].value == ";")
 						&& (tokens[tokens.size() - 1].value == ";"))
 				{
-					std::cout << "Error: antes de punto y coma no hay directiva" << std::endl;
+					std::cerr << "Error: antes de punto y coma no hay directiva" << std::endl;
 					return (false);
 				}
 			}
@@ -72,14 +69,6 @@ bool	tokenizeFile(const char *file, std::vector<t_token> &tokens, std::string &d
 	if (brackets != 0)
 		return (false);
 	return (true);
-}
-
-size_t	matrixLenChar(char const **matrix)
-{
-	size_t i;
-
-	for (i = 0; matrix[i] != NULL; i++);
-	return (i);
 }
 
 bool	validSubContextsCmp(std::string &context, std::string &subcontext)
@@ -108,13 +97,9 @@ bool	validSubContextsCmp(std::string &context, std::string &subcontext)
 bool	validDirectivesCmp(std::string	&context, std::string &directive)
 {
 	const char	*main[] = {"workers", NULL};
-	//const int	main_size[] = {0, -1};
 	const char	*events[] = {"prueba", NULL};
-	//const int	events_size[] = {0, -1};
 	const char	*http[] = {"hola", NULL};
-	//const int	http_size[] = {0, -1};
 	const char	*types[] = {"text/html", "text/css", "text/xml"};
-	//const int	types_size[] = {0, -1};
 	const char	*server[] = {"listen", "limit_body_size", "server_name", "timeout", "error_page", "root", "index", NULL};
 	const char	*location[] = {"autoindex", "limit_body_size", "root", "methods", "alias", "error_files", "index", "redirect", 
 								"try_files", "postdir", "cgi_pass", NULL};
@@ -141,52 +126,10 @@ bool	validDirectivesCmp(std::string	&context, std::string &directive)
 	}
 	return (false);
 }
-bool	strInVector(std::string &str, std::vector<std::string> &vector) //usar para comparar tanto directivas como contextos
-//hacer que en vez de devolver un bool devuelva un puntero, si no está es NULL, si está puedo acceder a él directamente
-{
-	//std::cout << "Buscar si str: " << str << " está en vector: " << std::endl; 
-	for (size_t i = 0; i < vector.size(); i++)
-	{
-		//std::cout << "A comparar: " << vector[i] << std::endl;
-		if (str == vector[i])
-			return (true);
-	}
-	return (false);
-}
-
-void	binaryInsert(std::vector<parseTree *> &vec, parseTree *insert) //hacer mejor un template de esto
-{
-	//std::cout << "Entro en binary insert" << std::endl;
-	if (vec.empty())
-	{
-		vec.push_back(insert);
-		return ;
-	}
-	int	res = binarySearch(vec, insert);
-	//std::cout << "Resultado de binarySearch: " << res << std::endl;
-	//hacer un switch
-	if (res == -1)
-	{
-		//std::cout << "Lo añade al principio" << std::endl;
-		vec.insert(vec.begin(), insert);
-	}
-	else if (res == -2)
-	{
-		//std::cout << "Lo añade al final o vector está vacío" << std::endl;
-		vec.push_back(insert);
-	}
-	else
-	{
-		//std::cout << "Guardar elemento en pos: " << res << std::endl;
-		vec.insert(vec.begin() + res, insert);
-	}
-}
-
 bool	parseContextTokens(parseTree *root, std::vector<t_token> &tokens)
 {
-	std::list<parseTree *>	nodes; //stack de nodos - se añade cuando entra a un contexto, se borra cuando termina, vuelve al anterior, evitar recursividad
-	parseTree	*child; //hijo del nodo del árbol a crear
-	//context		*subcontext; // subcontexto a crear y añadir al arbol
+	std::list<parseTree *>	nodes;
+	parseTree				*child;
 
 	nodes.push_front(root);
 	int	initDirective = 0;
@@ -202,21 +145,20 @@ bool	parseContextTokens(parseTree *root, std::vector<t_token> &tokens)
 			}
 			child = new parseTree();
 			child->context._name = tokens[initDirective].value;
-			//child->contextName = tokens[initDirective].value;
-			for (size_t start = initDirective + 1; start < i; start++) //añade argumentos del contexto si los hay
-				//child->contextName = tokens[initDirective].value;
+			for (size_t start = initDirective + 1; start < i; start++)
 				child->context._args.push_back(tokens[start].value);
 			if (root->context._name == "location")
-				binaryInsert(root->childs, child);
+				linearInsertLoc(root->childs, child);
 			else
 				root->childs.push_back(child);
-			//root->childsNames.push_back(child->contextName);
 			nodes.push_front(child);
 			root = child;
 			initDirective = i + 1;
 		}
 		else if (tokens[i].value == "}") //tiene que cerrar contexto y volver al anterior
 		{
+			if (tokens[i - 1].value != ";" && tokens[i - 1].value != "}")
+				return (false);
 			nodes.pop_front();
 			root = nodes.front();
 			initDirective = i + 1;
@@ -224,17 +166,11 @@ bool	parseContextTokens(parseTree *root, std::vector<t_token> &tokens)
 		else if (tokens[i].value == ";") // final de directiva, añadir a lista
 		{
 			endDirective = i;
-			//if (!validDirectivesCmp(root->contextName, tokens[initDirective].value))
 			if (!validDirectivesCmp(root->context._name, tokens[initDirective].value))
 			{
 				std::cout << "Directiva no está entre las posibles: " << tokens[initDirective].value << std::endl;
 				return (false);
 			}
-			//std::pair<std::string, std::vector<std::string> >	keyVal;
-			//keyVal.first = tokens[initDirective].value;
-			//for (int j = initDirective + 1; j < endDirective; j++) //añade values
-			//	keyVal.second.push_back(tokens[j].value);
-			//root->directives.push_back(keyVal); //añade key-values
 			for (int j = initDirective + 1; j < endDirective; j++)
 				root->context._dirs.insert(std::pair<std::string, std::string>(tokens[initDirective].value, tokens[j].value));
 			initDirective = i + 1;
@@ -244,16 +180,15 @@ bool	parseContextTokens(parseTree *root, std::vector<t_token> &tokens)
 }
 void	freeParseTree(parseTree *root)
 {
-	std::list<parseTree *>	freeNodes;
-	parseTree	*curr = root;
-	freeNodes.push_front(root);
-	while (!freeNodes.empty())
+	if (root->childs.empty())
 	{
-		for (size_t i = 0; i < curr->childs.size(); i++)
-				freeNodes.push_front(root);
-		curr = freeNodes.front();
-		free(curr);
-		freeNodes.pop_front();
+		delete(root);
+		return ;
+	}
+	for (size_t i = 0; i < root->childs.size(); i++)
+	{
+		freeParseTree(root->childs[i]);
+		delete(root->childs[i]);
 	}
 }
 
@@ -262,36 +197,14 @@ parseTree	*parseFile(char	*file)
 	std::string	del = "{};=";
 	std::vector<t_token>	tokens;
 	if (!tokenizeFile(file, tokens, del))
-	{
-		//std::cout << "Error al tokenizar" << std::endl;
 		return (NULL);
-	}
-	//for (size_t i = 0; i < tokens.size(); i++)
-	//	std::cout << "Token: " << tokens[i].value << std::endl;
 	parseTree	*root = new parseTree();
 	root->context._name = "main";
 	if (!parseContextTokens(root, tokens))
 	{
-		//llamar a funcion de liberar el arbol
 		freeParseTree(root);
+		system("leaks -q webserv");
 		return (NULL);
 	}
-	//std::cout << "---------------Imprimir árbol de fichero de configuración---------------" << std::endl;
-	//printBTree(root);
 	return (root);
 }
-
-/*int	main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		std::cout << "Error: bad number of arguments" << std::endl;
-		return (1);
-	}
-	if (!parseFile(argv[1]))
-	{
-		std::cout << "Hubo un error al parsear el fichero" << std::endl;
-		return (1);
-	}
-	return (0);
-}*/
---
+++ srcs/RESPONSE/getMethod.cpp
@@ -0,0 +1,113 @@
+#include "../../includes/webserv.hpp"
+
+static void	getIndex(client *client)
+{
+	if (!isInMultiMapKey(client->loc->context._dirs, "index"))
+		return ;
+	std::cout << "INDEX" << std::endl;
+	itr itk = client->loc->context._dirs.equal_range("index");
+
+	std::string	path;
+	std::vector<std::string>	redirs;
+	redirs.push_back("alias");
+	redirs.push_back("root");
+	for (itmap itb = itk.first, ite = itk.second; itb != ite; itb++)
+	{
+		path = getPathFileRequest(client, redirs) + itb->second;
+		if (!access(path.c_str(), F_OK | R_OK))
+		{
+			std::string body = getResponseBody(path);
+			client->response.response = getResponseHeader(client->request, body) + body;
+			client->request.status = 200;
+			return ;
+		}
+	}
+	throw (404);
+}
+
+static void	autoIndexListing(client *client)
+{
+	if (!isInMultiMapValue(client->loc->context._dirs, "autoindex", "on"))
+		return ;
+	std::cout << "AUTOINDEX" << std::endl;
+	std::vector<std::string>	redirsVec;
+	redirsVec.push_back("alias");
+	redirsVec.push_back("root");
+	std::string	path = getPathFileRequest(client, redirsVec);
+	std::cout << "Entro en autoindex" << std::endl;
+	DIR	*dir = opendir(path.c_str());
+	if (!dir)
+		throw (400);
+	dirent *elem = readdir(dir);
+	std::string body;
+	std::string	redirs[] = {"alias", "root"};
+	body += "<html><body>";
+	while (elem)
+	{
+		if (elem->d_name[0] != '.')
+		{
+			body += "<a href=\"http://";
+			body += *(getMultiMapValue(client->request.headers, "Host"));
+			body += "/" + getMultiMapValueKeys(client->loc->context._dirs, redirs, 2);
+			body += elem->d_name;
+			if (elem->d_type == DT_DIR)
+				body += '/';
+			body += "\">";
+			body += elem->d_name;
+			body += "\n</a>";
+		}
+		elem = readdir(dir);
+	}
+	body += "</body></html>";
+	closedir(dir);
+	client->request.status = 200;
+	client->response.response = getResponseHeader(client->request, body) + body;
+}
+
+static void pathIsDirectory(client *client)
+{
+	std::string dirs[] = {"autoindex", "index"};
+	void (*f[])(struct client *client) = {autoIndexListing, getIndex};
+
+	for (size_t i = 0; i < 2; i++)
+	{
+		if (getMultiMapValue(client->loc->context._dirs, dirs[i]))
+		{
+			f[i](client);
+			if (!client->response.response.empty())
+				return ;
+		}
+	}
+	throw (400);
+}
+
+static void	pathIsFile(client *client, std::string &path)
+{
+	if (access(path.c_str(), F_OK) != 0)
+		throw (404);
+	if (access(path.c_str(), R_OK) != 0)
+		throw (403);
+
+	HttpResponse Response;
+	client->request.status = 200;
+	Response.body = getResponseBody(path);
+	Response.firstLine = getResponseHeader(client->request, Response.body);
+	client->response.response = Response.firstLine + Response.body;
+}
+
+void	getMethod(client *client)
+{
+	std::cout << "GET METHOD" << std::endl;
+
+	std::vector<std::string>	redirs;
+	redirs.push_back("alias");
+	redirs.push_back("root");
+	std::string	path = getPathFileRequest(client, redirs);
+	struct stat	st;
+	if (stat(path.c_str(), &st) == -1)
+		throw (404);
+	if (st.st_mode & S_IFDIR)
+		pathIsDirectory(client);
+	else
+		pathIsFile(client, path);
+}
